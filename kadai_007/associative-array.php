4a19e6bf17cb90da7c6805118780ceb0032508be
mihoakaike <akaike.miho.00@gmail.com>
2024-01-20 14:54:51 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<html lang="ja">
+ 
+    <head>
+     <meta charset="UTF-8">
+    </head>
+     
+    <body>
+    <p>
+        <?php
+
+            $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160,];
+
+            print_r($product_data);
+            
+        ?>
+    </p>
+    </body>
+ 
+ </html>
